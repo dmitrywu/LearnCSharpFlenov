f135025d5fe5ba4fe1a6be4e6b6508f2194193ec
Dmitry <dmitry.wu@gmail.com>
2024-04-10 06:09:37 +0300

    V1E2 complete

---
+++ V1E2/V1E2/Program.cs
@@ -0,0 +1,129 @@
+﻿Console.Clear();
+//ConsoleColor consoleColor = ConsoleColor.Blue;
+//Console.ForegroundColor = consoleColor;
+//Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+int i = 10;
+Console.WriteLine(i == 10 ? "Cool" : "Bad");
+
+//int i = 2;
+//bool b=true;
+
+//switch(i)
+//{
+//    case 1:
+//        Console.WriteLine("1");
+//        break; //return;
+//    case 2:
+//        Console.WriteLine("2");
+//        return;
+//case 3:
+//        Console.WriteLine("3");
+//        break;
+//        default:
+//        Console.WriteLine("default");
+//        break;
+
+//}
+
+//Console.WriteLine(i);
+
+//Console.WriteLine(b);
+
+//if (i > 10)
+//{
+//    Console.WriteLine(i);
+//}
+//else if (i <0)
+//{
+//    Console.WriteLine("Отрицательное");
+//}
+//else
+//{
+//    Console.WriteLine("Больше");
+//}
+
+
+//int i = 110;
+//bool b = i != 100;
+//Console.WriteLine(b);
+//if (i>10 && i<1000) // <= == >= !=  && ||
+//{
+//    Console.WriteLine("Меньше 100");
+//    i += 10;
+//    Console.WriteLine(i);
+//}
+//else
+//{
+//    i -= 40;
+//    Console.WriteLine("Больше");
+//}
+//i = i + 210;
+//Console.WriteLine($"{i}");
+
+
+
+// See https://aka.ms/new-console-template for more information
+
+//int a = 20;
+//double a = 20;
+//int b = 15;
+
+//int sum;
+
+//double sum;
+
+//sum=a/b;  // + - * /
+
+//sum++; 
+// sum--;
+
+//int i = 10;
+//i = i + 5;
+//i += 5;
+//i = i - 5;
+// i *=5;
+//i -= 5;
+
+//Console.WriteLine($"{sum}");
+//Console.WriteLine($"{i}");
+
+//string s1 = "Hello";
+//string s2 = "World";
+//string r; 
+
+//r = s1 +" "+ s2;
+
+//Console.WriteLine($"{r}");
+
+//int a = 10;
+//int b = ++a;
+
+//int a = 2;
+//int sum;
+
+//sum = a + a*2;
+//sum = (a + a) * 2;
+
+//bool b = true;
+//Boolean b1 = false;
+//Console.WriteLine($"{b1}");
+
+//Console.WriteLine($"{sum}");
+
+//int i = 110;
+//if (i < 100) // <= == >= !=
+//{
+//    Console.WriteLine("Меньше 100");
+//    i += 10;
+//    Console.WriteLine(i);
+//}
+//else
+//{
+//    i -= 40;
+//    Console.WriteLine("Больше"); }
+//i = i + 210;
+//Console.WriteLine($"{i}");
+
+//Console.WriteLine($"{b1}");
