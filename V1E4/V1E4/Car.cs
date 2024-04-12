c0ef54be41a24b55b9c58f69e37384147cf1837c
Dmitry <dmitry.wu@gmail.com>
2024-04-12 05:36:31 +0300

    V1E4 completed

---
+++ V1E4/V1E4/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TestProject
+{
+    public class Car
+    {
+        private int width = 10;
+        public int Width { 
+            get { return width; }
+            set { 
+                if (value > 0) 
+                    width = value; 
+            }
+        }
+        public int Height { get; set; } = 20;
+        public int Color { get; set; }
+
+        public int Size {
+            get { return Width * Height; }
+        }
+
+        public int GetSize()
+        { 
+            return Width * Height;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Start");
+        }
+    }
+}
---
+++ V1E4/V1E4/Program.cs
@@ -0,0 +1,25 @@
+﻿using TestProject;
+
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+Car vasinaMashina = new Car();
+Car petinaMashina = new Car();
+
+vasinaMashina.Width = 10;
+vasinaMashina.Height = 5;
+
+petinaMashina.Width = -9;
+petinaMashina.Height = 4;
+
+Console.WriteLine(vasinaMashina.Height);
+Console.WriteLine(petinaMashina.Height);
+
+vasinaMashina.Start();
+petinaMashina.Start();
+
+Console.WriteLine($"Size = {vasinaMashina.GetSize()}");
+Console.WriteLine($"Size = {petinaMashina.GetSize()}");
+
+Car car3 = new Car();
+Console.WriteLine(car3.GetSize());
