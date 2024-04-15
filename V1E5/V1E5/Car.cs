922deebb687399508753fb85c3e58cee60113237
Dmitry <dmitry.wu@gmail.com>
2024-04-15 04:58:42 +0300

    V1E5 completed

---
+++ V1E5/V1E5/Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace TestProject
+{
+    public class Car
+    {
+        private int gear = 0;
+
+        public int Gear
+        {
+            get { return gear; }
+        }
+
+        private int width = 10;
+        public int Width
+        {
+            get { return width; }
+            set
+            {
+                if (value > 0)
+                    width = value;
+            }
+        }
+        public int Height { get; set; } = 2;
+        public int Color { get; set; }
+
+        public int Size
+        {
+            get { return Width * Height; }
+        }
+
+        public int GetSize()
+        {
+            return Width * Height;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Start");
+        }
+
+        public void SetGear(int gear)
+        {
+            this.gear = gear;
+        }
+
+        public int SetSize(int width)
+        {
+            return width;
+
+        }
+
+        public int SetSize(ref int width, ref int height, out int newheight)
+        {
+            this.width = width;
+            this.Height = height;
+            newheight = height;
+            width = 20;
+            return this.width;
+        }
+    }
+}
---
+++ V1E5/V1E5/Program.cs
@@ -0,0 +1,17 @@
+﻿using TestProject;
+
+Car vasinacar = new Car();
+vasinacar.SetGear(2);
+
+Car petinacar = new Car();
+Console.WriteLine(petinacar.Gear);
+petinacar.SetGear(3);
+Console.WriteLine(petinacar.Gear);
+
+int newheight;
+int w = 9;
+int h = 1;  
+int newwidth = petinacar.SetSize(ref w, ref h, out newheight);
+Console.WriteLine($"{newheight}x{newwidth}");
+Console.WriteLine(w);
+
