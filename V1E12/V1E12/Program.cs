e23b2e22fe0951a9ed32ea7a63c31962d99b7c12
Dmitry <dmitry.wu@gmail.com>
2024-04-24 05:05:34 +0300

    V1E12 at 13:50

---
+++ V1E12/V1E12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+ArrayList arrayList = new ArrayList();
+arrayList.Add(10);
+arrayList.Add(1);
+Console.WriteLine(arrayList.Count);
+arrayList.Add(3);
+Console.WriteLine(arrayList.Count);
+
+//int[] m = new int[10];
+
+//int[] i = { 10, 20 };
+//i[0] = 1;
+//i[1] = 2;
+
+//Array.Clear(m, 0, 10);
+//m[0] = 0;
+//m[5] = 1;
+//m[3] = -9;
+//Array.Sort(m);
+//int myd = Array.BinarySearch(m, 1);
+
+
+//Console.WriteLine(myd);
+
+//for (int i = 0; i < m.Length; i++)
+//{
+//    Console.WriteLine(m[i]);
+
+//}
+
+
+//int[][] m = new int[10][];
+//m[0] = new int[5];
+//m[1] = new int[2];
+
+//m[1][4] = 10;
