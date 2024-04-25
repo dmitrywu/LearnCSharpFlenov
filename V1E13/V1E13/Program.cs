b9fc752029e42027201baf9b85eefcc3f133b772
Dmitry <dmitry.wu@gmail.com>
2024-04-25 05:16:57 +0300

    V1E13 complete

---
+++ V1E13/V1E13/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+trydiv(4, 2);
+trydiv(4, 0);
+
+
+while (true)
+{
+    Console.WriteLine("Enter number: ");
+    string line = Console.ReadLine();
+    if (line == "q") break;
+
+    try
+    {
+        int number = Int32.Parse(line);
+        // int number = Convert.ToInt32(line);
+        Console.WriteLine("num: " + number.ToString());
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Incorrect number");
+    }
+    //catch(ArgumentNullException) {
+    //    Console.WriteLine();
+    //}
+}
+
+//SystemException
+//ApplecationException
+
+void trydiv(int x, int y)
+{
+    try
+    {
+        Console.WriteLine(div(x, y));
+    }
+    catch (DivideByZeroException e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    catch (MyDivByZeroException e)
+    {
+        Console.WriteLine($"Do not devide {e.x} by {e.y}");
+            }
+}
+
+double div(int x, int y)
+{
+    if (y == 0)
+        throw new MyDivByZeroException(x, y);
+
+    return x / y;
+}
+
+public class MyDivByZeroException : ApplicationException
+{
+    public int x { get; set; }
+    public int y { get; set; }
+    public MyDivByZeroException(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+
+    }
+
+}
